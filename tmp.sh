6cd0ff8d323c745ed6feefc64eb2b9e7cc515242
RuiLei <ruileisurprise@gmail.com>
2019-12-30 17:28:54 +0800

    dd

--- tmp.sh
+++ tmp.sh
@@ -35,7 +35,7 @@
 #       set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
 #   MAVEN_SKIP_RC - flag to disable loading of mavenrc files
 # ----------------------------------------------------------------------------
-
+eee
 if [ -z "$MAVEN_SKIP_RC" ] ; then
 
   if [ -f /etc/mavenrc ] ; then
